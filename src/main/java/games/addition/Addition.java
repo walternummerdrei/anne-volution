d23565e2a5b5fb06a78efaa026cea5c2e8879faf
jharho <j.harhoff@tarent.de>
2018-03-15 17:19:14 +0100

    Game Addition: Can now add binary numbers

--- src/main/java/games/addition/Addition.java
+++ src/main/java/games/addition/Addition.java
@@ -12,43 +12,60 @@ public class Addition {
     public static void main(String[] args) {
         LinkedList<Anne> annes = new LinkedList<>();
 
-        int amountOfAnnes = 50;
+        int amountOfAnnes = 100;
+        int generations = 100;
+        int turnsPerGeneration = 10;
 
         for (int i = 0; i < amountOfAnnes; i++) {
-            annes.add(new Anne(6, 3, 10, 1));
+            annes.add(new Anne(6, 5, 10, 1));
         }
 
         LinkedList<Double> inputs = new LinkedList<>();
-        Map<Double, Anne> results = new HashMap<>();
+        Map<Anne, Double> results = new HashMap<>();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < generations; i++) {
+            results.clear();
+            annes.forEach(anne -> results.put(anne, 0D));
 
-            int number1 = i % 8;
-            int number2 = (i + 1) % 8;
+            for (int j = 0; j < turnsPerGeneration; j++) {
 
-            // number 1
-            inputs.add(i % 8 >= 4 ? 1D : 0D);
-            inputs.add(i % 4 >= 2 ? 1D : 0D);
-            inputs.add(i % 2 >= 1 ? 1D : 0D);
+                inputs.clear();
 
-            // number 2
-            inputs.add(i + 1 % 8 >= 4 ? 1D : 0D);
-            inputs.add(i + 1 % 4 >= 2 ? 1D : 0D);
-            inputs.add(i + 1 % 2 >= 1 ? 1D : 0D);
+                int number1 = j % 8;
+                int number2 = (j + 1) % 8;
 
-            annes.parallelStream().forEach(anne -> results.put(anne.processInputs(inputs).getFirst(), anne));
+                // number 1
+                inputs.add(j % 8 >= 4 ? 1D : 0D);
+                inputs.add(j % 4 >= 2 ? 1D : 0D);
+                inputs.add(j % 2 >= 1 ? 1D : 0D);
 
-            Map.Entry<Double, Anne> winner = results.entrySet().stream().reduce((r1, r2) -> Math.abs(r1.getKey() - (number1 + number2)) > Math.abs(r2.getKey() - (number1 + number2)) ? r2 : r1).get();
+                // number 2
+                inputs.add(j + 1 % 8 >= 4 ? 1D : 0D);
+                inputs.add(j + 1 % 4 >= 2 ? 1D : 0D);
+                inputs.add(j + 1 % 2 >= 1 ? 1D : 0D);
 
-            Anne bestAnne = winner.getValue();
+                results.entrySet().parallelStream()
+                        .forEach(entry -> {
+                            Anne anne = entry.getKey();
+                            double oldDeviation = entry.getValue();
+                            double additionalDeviation = number1 + number2 - anne.processInputs(inputs).getFirst();
+                            entry.setValue(oldDeviation + additionalDeviation);
+                        });
+            }
 
-            System.out.println("Result:          " + (number1 + number2));
-            System.out.println("Best difference: " + ((number1 + number2) - winner.getKey()));
+            Map.Entry<Anne, Double> winner = results.entrySet().stream().reduce((r1, r2) -> Math.abs(r1.getValue()) < Math.abs(r2.getValue()) ? r1 : r2).get();
+            Anne bestAnne = winner.getKey();
+
+            System.out.println("Generation:     " + i);
+            System.out.println("Best deviation: " + (winner.getValue()));
             System.out.println("");
 
-            for (int j = 0; j < amountOfAnnes; j++) {
-                annes.add(bestAnne);
+            annes.clear();
+
+            for (int k = 0; k < amountOfAnnes; k++) {
+                annes.add(new Anne(bestAnne));
             }
+
         }
 
         System.out.println("DONE");
